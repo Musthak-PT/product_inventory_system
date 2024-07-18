7b955ca09f776662aa9943db33f46fd7f3f91482
Musthak <musthak@aventusinformatics.com>
2024-07-18 11:42:49 +0530

    initial setuping and model creation

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from products.models import Product , Variant , SubVariant
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Variant)
+admin.site.register(SubVariant)
--- products/models.py
+++ products/models.py
@@ -5,18 +5,18 @@ from versatileimagefield.fields import VersatileImageField
 from django.contrib.auth.models import User
 
 class Product(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    ProductID = models.BigIntegerField(unique=True)
-    ProductCode = models.CharField(max_length=255, unique=True)
-    ProductName = models.CharField(max_length=255)
-    ProductImage = VersatileImageField(upload_to="uploads/", blank=True, null=True)
-    CreatedDate = models.DateTimeField(auto_now_add=True)
-    UpdatedDate = models.DateTimeField(blank=True, null=True)
-    CreatedUser = models.ForeignKey(User, related_name="user_products", on_delete=models.CASCADE)
-    IsFavourite = models.BooleanField(default=False)
-    Active = models.BooleanField(default=True)
-    HSNCode = models.CharField(max_length=255, blank=True, null=True)
-    TotalStock = models.DecimalField(default=0.00, max_digits=20, decimal_places=8, blank=True, null=True)
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    ProductID       = models.BigIntegerField(unique=True)
+    ProductCode     = models.CharField(max_length=255, unique=True)
+    ProductName     = models.CharField(max_length=255)
+    ProductImage    = VersatileImageField(upload_to="uploads/", blank=True, null=True)
+    CreatedDate     = models.DateTimeField(auto_now_add=True)
+    UpdatedDate     = models.DateTimeField(blank=True, null=True)
+    CreatedUser     = models.ForeignKey(User, related_name="user_products", on_delete=models.CASCADE)
+    IsFavourite     = models.BooleanField(default=False)
+    Active          = models.BooleanField(default=True)
+    HSNCode         = models.CharField(max_length=255, blank=True, null=True)
+    TotalStock      = models.DecimalField(default=0.00, max_digits=20, decimal_places=8, blank=True, null=True)
 
     class Meta:
         db_table = "products_product"
@@ -26,10 +26,10 @@ class Product(models.Model):
         ordering = ("-CreatedDate", "ProductID")
 
 class Variant(models.Model):
-    product = models.ForeignKey(Product, related_name='variants', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    product   = models.ForeignKey(Product, related_name='variants', on_delete=models.CASCADE)
+    name      = models.CharField(max_length=255)
 
 class SubVariant(models.Model):
-    variant = models.ForeignKey(Variant, related_name='sub_variants', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    stock = models.DecimalField(default=0.00, max_digits=20, decimal_places=8)
+    variant   = models.ForeignKey(Variant, related_name='sub_variants', on_delete=models.CASCADE)
+    name      = models.CharField(max_length=255)
+    stock     = models.DecimalField(default=0.00, max_digits=20, decimal_places=8)

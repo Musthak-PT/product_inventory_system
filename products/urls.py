4230fb4395c8c45e44d9a14cbd6786ae9a4e51dc
Musthak <musthak@aventusinformatics.com>
2024-07-18 11:27:56 +0530

    initial setups

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'products'
+
+urlpatterns = [
+      
+    # path('listing-of-milestones/', views.MilestoneListingApiView.as_view(), name='listing-milestone'),
+    
+]

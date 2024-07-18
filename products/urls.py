8032752d87a2532e9f4c69f446e6cc80e4b9a8ab
Musthak <musthak@aventusinformatics.com>
2024-07-18 15:04:04 +0530

    completed the apis

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ app_name = 'products'
 
 urlpatterns = [
       
-    path('products/create/', views.ProductCreateOrUpdateApiView.as_view(), name='product-create-or-update'),
+    path('products/create_or_update/', views.ProductCreateOrUpdateApiView.as_view(), name='product-create-or-update'),
     path('products/list/', views.ProductListingApiView.as_view(), name='listing-products'),
     path('subvariant/<pk>/add_stock/', views.AddStockView.as_view(), name='add_stock'),
     path('subvariant/<pk>/remove_stock/', views.RemoveStockView.as_view(), name='remove_stock'),

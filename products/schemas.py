faf290a06ed8182bca3e78701968faa0abddaacc
Musthak <musthak@aventusinformatics.com>
2024-07-18 13:57:33 +0530

    completed the listing of products

--- products/schemas.py
+++ products/schemas.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
-from products.models import Product
+from products.models import Product, Variant, SubVariant
+
+class SubVariantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubVariant
+        fields = ['id', 'name', 'stock']
+
+class VariantSerializer(serializers.ModelSerializer):
+    sub_variants = SubVariantSerializer(many=True)
+
+    class Meta:
+        model = Variant
+        fields = ['id', 'name', 'sub_variants']
 
 class ProductResponseSchema(serializers.ModelSerializer):
+    variants = VariantSerializer(many=True)
+
     class Meta:
-        model= Product
+        model = Product
         fields = '__all__'
-        
+
     def to_representation(self, instance):
         datas = super().to_representation(instance)
         for key in datas.keys():
